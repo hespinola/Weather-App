e4e29964249baf5dfab63bf9998e2755ad5ae297
Humberto Espinola <h.espinola@icloud.com>
2016-09-02 17:37:18 -0500

    Added elements to the main view

---
+++ WeatherApp/MainVC.swift
@@ -0,0 +1,35 @@
+//
+//  MainVC.swift
+//  WeatherApp
+//
+//  Created by Isomi on 9/2/16.
+//  Copyright © 2016 Isomi. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
+
